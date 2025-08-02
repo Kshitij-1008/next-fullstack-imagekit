a5772a236b3f6f99acdc3de96da79f98df2f5060
Kshitij-1008 <kshitijjha1008@gmail.com>
2025-08-02 19:34:55 +0530

    Added notification handling

---
+++ app/components/Header.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import { Home, User } from "lucide-react";
+
